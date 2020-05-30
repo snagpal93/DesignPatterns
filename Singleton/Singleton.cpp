51ac68978a3e44975946458afe9eb36ba296f852
snagpal93 <saharsh.nagpal93@gmail.com>
2020-05-30 11:09:42 +0200

    Simple implementationof non-thread safe singleton.

--- Singleton/Singleton.cpp
+++ Singleton/Singleton.cpp
@@ -1,7 +1,45 @@
+/*
+Simple Example of Singleton Design Pattern. This implementation is not thread safe and uses lazy initialization.
+This pattern is useful if we want only one instance of an object to exist throughout the code. For example: Config, Registry settings.
+*/
+
 #include <iostream>
 
 using namespace std;
 
+class Singleton {
+    private:
+        Singleton() {
+            val = "INITIALIZE";
+        }
+        static Singleton* singleton;
+        std::string val;
+
+    public:
+        Singleton(Singleton &copy) = delete;
+        void operator=(const Singleton&) = delete;
+
+        static Singleton *GetInstance() ;
+
+        std::string getVal() const{
+            return val;
+        }
+        
+};
+
+Singleton* Singleton::singleton = NULL;
+
+Singleton *Singleton::GetInstance() {
+    if (singleton==NULL) {
+        singleton = new Singleton();
+    }
+    return singleton;
+}
+
 int main(){
-    cout <<"hello World"<<endl;
+    Singleton *instance = Singleton::GetInstance();
+    
+    cout<<instance->getVal()<<endl;
+
+    return 0;
 }
